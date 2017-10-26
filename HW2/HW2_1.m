bf2103d13e016eb8d2771ae7197f5f682f806d5a
martian491 <martian491@gmail.com>
2017-10-26 11:04:55 +0800

    Finish HW2

---
+++ HW2/HW2_1.m
@@ -0,0 +1,39 @@
+% HW2_1
+
+clear;clc;
+
+% x(t) Initial condition
+   t0 = 0;
+   x0 = 0;
+xdot0 = 0;
+
+   t1 = 1;
+   x1 = 3;
+xdot1 = 0;
+
+% Determin Cubi Polynomial Matrix Coefficient Vector
+[a,b,c,d] = CubiPolynomialVector(t0,t1,x0,xdot0,x1,xdot1);
+
+    position = @(t)a*t.^3 + b*t.^2 + c*t + d;
+    velocity = @(t)     3*a*t.^2 + 2*b*t + c;
+acceleration = @(t)              3*a*t + 2*b;
+
+t_interval = linspace(0,1.5);
+
+subplot(3,1,1);
+plot(t_interval,position(t_interval));
+xlabel('Time(s)')
+ylabel('x(t)')
+axis([-inf,inf,0,4]);
+
+subplot(3,1,2);
+plot(t_interval,velocity(t_interval));
+xlabel('Time(s)')
+ylabel('v(t)')
+axis([-inf,inf,-15,7]);
+
+subplot(3,1,3);
+plot(t_interval,acceleration(t_interval));
+xlabel('Time(s)')
+ylabel('a(t)')
+
---
+++ HW2/HW2_2.m
@@ -0,0 +1,72 @@
+% HW2_2
+
+clear;clc;
+
+%  Initial condition
+            t = [0;
+                 2;
+                 3;
+                 4];
+            
+   postion  = [0,2.0,0.5,0.0;
+               0,1.0,2.0,0.5;
+               0,1.0, pi, pi];
+           
+   velocity = [0,0.5, 1.0,0;
+               0,1.0,-0.5,0;
+               0,1.0, 0.5,0;];
+            
+% Determin Cubi Polynomial Matrix Coefficient Vector Seg1
+for seg = 1:1:3
+            [xa,xb,xc,xd] = CubiPolynomialVector(t(seg,1),t(seg+1,1),postion(1,seg),velocity(1,seg),postion(1,seg+1),velocity(1,seg+1));
+            [ya,yb,yc,yd] = CubiPolynomialVector(t(seg,1),t(seg+1,1),postion(2,seg),velocity(2,seg),postion(2,seg+1),velocity(2,seg+1));
+    [phia,phib,phic,phid] = CubiPolynomialVector(t(seg,1),t(seg+1,1),postion(3,seg),velocity(3,seg),postion(3,seg+1),velocity(3,seg+1));
+    
+          x_position = @(t)xa*t.^3 + xb*t.^2 + xc*t + xd;
+          x_velocity = @(t)      3*xa*t.^2 + 2*xb*t + xc;
+      x_acceleration = @(t)                3*xa*t + 2*xb;
+ 
+          y_position = @(t)ya*t.^3 + yb*t.^2 + yc*t + yd;
+          y_velocity = @(t)      3*ya*t.^2 + 2*yb*t + yc;
+      y_acceleration = @(t)                3*ya*t + 2*yb;
+
+               angle = @(t)phia*t.^3 + phib*t.^2 + phic*t + phid;
+    angular_velocity = @(t)        3*phia*t.^2 + 2*phib*t + phic;
+angular_acceleration = @(t)                    3*phia*t + 2*phib;
+
+      t_interval = linspace(t(seg,1),t(seg+1,1));
+      
+      subplot(3,1,1)
+      plot(t_interval,x_position(t_interval),'b');
+      hold on
+      plot(t_interval,y_position(t_interval),'g');
+      hold on
+      plot(t_interval,angle(t_interval),'r');
+      hold on
+      grid
+      
+      subplot(3,1,2)
+      plot(t_interval,x_velocity(t_interval),'b');
+      hold on
+      plot(t_interval,y_velocity(t_interval),'g');
+      hold on
+      plot(t_interval,angular_velocity(t_interval),'r');
+      hold on
+      grid
+      
+      subplot(3,1,3)
+      plot(t_interval,x_acceleration(t_interval),'b');
+      hold on
+      plot(t_interval,y_acceleration(t_interval),'g');
+      hold on
+      plot(t_interval,angular_acceleration(t_interval),'r');
+      hold on
+      grid
+    
+end
+
+
+
+
+
+

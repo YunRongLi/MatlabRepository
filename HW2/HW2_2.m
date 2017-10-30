1499b24eeeaddde1e45d81099c1bd676ed02055d
martian491 <martian491@gmail.com>
2017-10-30 21:48:01 +0800

    Change equation vector

--- HW2/HW2_2.m
+++ HW2/HW2_2.m
@@ -16,22 +16,22 @@ clear;clc;
                0,1.0,-0.5,0;
                0,1.0, 0.5,0;];
             
-for seg = 1:1:3
+for seg = 1:1:2
             [xa,xb,xc,xd] = CubiPolynomialVector(t(seg,1),t(seg+1,1),postion(1,seg),velocity(1,seg),postion(1,seg+1),velocity(1,seg+1));
             [ya,yb,yc,yd] = CubiPolynomialVector(t(seg,1),t(seg+1,1),postion(2,seg),velocity(2,seg),postion(2,seg+1),velocity(2,seg+1));
     [phia,phib,phic,phid] = CubiPolynomialVector(t(seg,1),t(seg+1,1),postion(3,seg),velocity(3,seg),postion(3,seg+1),velocity(3,seg+1));
     
           x_position = @(t)xa*power(t,3) + xb*power(t,2) + xc*t + xd;
           x_velocity = @(t)3*xa*power(t,2) + 2*xb*t + xc;
-      x_acceleration = @(t)3*xa*t + 2*xb;
+      x_acceleration = @(t)6*xa*t + 2*xb;
  
           y_position = @(t)ya*power(t,3) + yb*power(t,2) + yc*t + yd;
           y_velocity = @(t)3*ya*power(t,2) + 2*yb*t + yc;
-      y_acceleration = @(t)3*ya*t + 2*yb;
+      y_acceleration = @(t)6*ya*t + 2*yb;
 
                angle = @(t)phia*power(t,3) + phib*power(t,2) + phic*t + phid;
     angular_velocity = @(t)3*phia*power(t,2) + 2*phib*t + phic;
-angular_acceleration = @(t)3*phia*t + 2*phib;
+angular_acceleration = @(t)6*phia*t + 2*phib;
 
       t_interval = linspace(t(seg,1),t(seg+1,1));
    
@@ -92,6 +92,7 @@ angular_acceleration = @(t)3*phia*t + 2*phib;
       ylabel('Angular Acceleration(rad/sec^2)');
       plot(t_interval,angular_acceleration(t_interval),'r');
       hold on
+      
       legend({'$\ddot{\phi}(t)$'},'Interpreter','latex','FontSize',16);
       grid
 end
